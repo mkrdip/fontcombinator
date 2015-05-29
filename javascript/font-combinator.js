33cac28c25110634a3eff0c0f2fd2f607f8820fb
Chip Cullen <chip@chipcullen.com>
2015-05-29 13:54:39 -0400

    Organized the file, got Font <option>s added

--- javascript/font-combinator.js
+++ javascript/font-combinator.js
@@ -3,7 +3,7 @@
 
 // The file is organized into the following sections
 
-// SETUP - inital creation of namespace
+// SETUP & UTILITIES - inital creation of namespace, utility functions that are used elsewhere
 // ASSEMBLE - building pieces of the page
 // RESPOND - responding to user events
 // SAVE STATE - saving things to localStorage
@@ -13,14 +13,49 @@
 // START - starting everything up
 
 
-/* ______ SETUP _______ */
+/* ______ SETUP & UTILITIES _______ */
 
 // using the fc object to namespace everything
 var fc = {
   html: document.querySelector('html'),
   body: document.querySelector('body'),
   content: document.querySelector('.content'),
+  apiKey: 'AIzaSyAc3a2WfPaSbA1B25u78zFQRfAide8T34c',
+  systemFonts: new Array(
+    'Arial',
+    'Garamond',
+    'Georgia',
+    'Helvetica',
+    'Palatino',
+    'Tahoma',
+    'Times New Roman',
+    'Trebuchet MS',
+    'Verdana'
+    ),
+  elems: new Array(
+    'h1',
+    'h2',
+    'p'
+    ),
+  checkForSystemFont: function(fontName) {
+    if(fc.systemFonts.indexOf(fontName) !== -1){
+      return true;
+    }
+  },
+  changeSpacesToPluses: function(str) {
+    var str = str.replace(/\ /g,'+');
+    return str;
+  },
+  changePlusesToSpaces: function(str) {
+    var str = str.replace(/\+/g,' ');
+    return str;
+  }
+
 };
+
+
+
+
 
 var Config = function () {
   var fontFamily,
@@ -42,18 +77,46 @@ fc.breakArrayIntoGroups = function (data, maxPerGroup) {
   return groups;
 }
 
+fc.addFontOptions = function(fontList) {
+
+  var fontSelects = document.querySelectorAll('#h1Font, #h2Font, #pFont');
+
+  var seperator = document.createElement('option');
+  seperator.disabled = "disabled";
+  seperator.innerHTML = "-- System Fonts --";
+
+  for (var j = fontSelects.length - 1; j >= 0; j--)  {
+      var thisSelect = fontSelects[j];
+      thisSelect.insertBefore(seperator.cloneNode(true), thisSelect.firstChild);
+  };
+
+  for (var i = fontList.length - 1; i >= 0; i--) {
+    // for now, limiting this list to just latin fonts
+    if (fontList[i].subsets.indexOf('latin') !== -1){
+      var newOption = document.createElement('option');
+      newOption.value = fontList[i].family;
+      newOption.innerHTML = fontList[i].family;
+    } 
+
+    for (var j = fontSelects.length - 1; j >= 0; j--)  {
+        var thisSelect = fontSelects[j];
+        thisSelect.insertBefore(newOption.cloneNode(true), thisSelect.firstChild);
+    };
+
+  };
+}
 
 fc.requestFonts = function (fontList) {
   var base = "http://fonts.googleapis.com/css?family=";
   var groupedFontList = fc.breakArrayIntoGroups(fontList, 6);
 
-  console.log(groupedFontList);
+  //console.log(groupedFontList);
 
   for (var i=0, j = groupedFontList.length; i < j; i++) {
     //console.log(groupedFontList[i].length);
 
     for (var k=0, l = groupedFontList[i][k].length; k < l; k++) {
-      console.log(groupedFontList[i][k]);
+      //console.log(groupedFontList[i][k]);
     }
   }
 
@@ -63,15 +126,24 @@ fc.requestFonts = function (fontList) {
 /* ______ RESPOND _______ */
 
 // this changes the on-page styling
-fc.changeStyles = function (){
+fc.changeStyles = function () {
   var newValue = this.value;
   var target = document.querySelector('.content ' + this.getAttribute('data-target'));
   var property = this.getAttribute('data-property');
 
-  // variant is a special case 
-  if (property === 'variant'){
+  if (property === 'fontFamily') {
+    if (!fc.checkForSystemFont(newValue)) {
+      // adding Google Font Link here
+      console.log('yo');
+      
+    } 
+    console.log( fc.changeSpacesToPluses('will need to add a font link') );
+    target.style[property] = newValue;
+
+  } else if (property === 'variant'){
+    // variant is a special case 
     console.log(property);
-    
+
   } else {
     //everthing else is pretty straight forward
     target.style[property] = newValue;
@@ -113,7 +185,7 @@ fc.intialDOMManipulation = function () {
 fc.apiCall = function () {
   var request = new XMLHttpRequest();
 
-  var apiURL = 'https://www.googleapis.com/webfonts/v1/webfonts?key=AIzaSyAc3a2WfPaSbA1B25u78zFQRfAide8T34c&sort=alpha&sort=desc';
+  var apiURL = 'https://www.googleapis.com/webfonts/v1/webfonts?key='+ fc.apiKey +'&sort=alpha&sort=asc';
 
   request.open('GET', apiURL, true);
 
@@ -122,6 +194,8 @@ fc.apiCall = function () {
       // Success!
       //var resp = this.response;
         var data = JSON.parse(this.response);
+        console.log(data);
+        fc.addFontOptions(data.items);
         fc.requestFonts(data.items);
     } else {
       // We reached our target server, but it returned an error
